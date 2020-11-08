165c55e4d29af53e6404dc70d783ee23be1f70c0
Amadou <infos@webamadou.com>
2020-11-08 10:18:07 +0000

    adding company name key. add cancel page

---
+++ resources/views/home/canceled.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.front_layout')
+@section('container')
+    <div class="container">
+        <h1 class="mt-5 text-center"><i class="fa fa-times-circle" style="color: #ff3300"></i> Votre commande est annulée </h1>
+    </div>
+@endsection
